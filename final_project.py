0561d9243fcb681af117a1665a529d184a9d12f2
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-28 23:49:36 +0800

    create def function

--- final_project.py
+++ final_project.py
@@ -6,3 +6,11 @@ from final_project_try import send_email
 SHEET_ID = "1Aw-sAHaWse4pELT8KA5mvsL1HGElHnSJj9p8Xv0cnog"
 SHEET_NAME = "Sheet1"
 URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
+
+def load_df(url):
+    parse_dates = ["due_date", "reminder_date"]
+    df = pd.read_csv(url, parse_dates=parse_dates)
+    return df
+
+
+print(load_df(URL))
