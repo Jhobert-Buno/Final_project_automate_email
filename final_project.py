05fc9afa40427a9dc712415cdc4c691d347f8fd7
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-29 00:35:01 +0800

    create a sending email function

--- final_project.py
+++ final_project.py
@@ -12,5 +12,19 @@ def load_df(url):
     df = pd.read_csv(url, parse_dates=parse_dates)
     return df
 
-
-print(load_df(URL))
+# create a sending email function
+def query_data_and_send_emails(df):
+    present = date.today()
+    email_counter = 0
+    for _, row in df.iterrows():
+        if (present >= row["reminder_date"].date()) and (row["has_paid"] == "no"):
+            send_email(
+                subject = f'[Coding is Fun] Invoice: {row["invoice_no"]}',
+                receiver_email = row["email"],
+                name = row["name"],
+                due_date = row["due_date"].strftime("%d, %b %Y"),
+                invoice_no = row["invoice_no"],
+                amount = row["amount"], 
+            )
+            email_counter += 1
+    return f"Total Emails Sent: {email_counter}"
