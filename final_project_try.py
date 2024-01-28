b57983166e44f7339179da2bc5ddc179dc8d7aa5
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-28 20:38:22 +0800

    create the message (to change)

--- final_project_try.py
+++ final_project_try.py
@@ -19,6 +19,26 @@ load_dotenv(envars)
 sender_email = os.getenv("email")
 password_email = os.getenv("password")
 
+def sender_email(subject, reciever_email, name, due_date, invoice_no, amount):
+    #Create the base text message
+    message = EmailMessage()
+    message["Subject"] = subject
+    message["From"] = formataddr(("Jhobert Buño", f"{sender_email}"))
+    message["To"] = reciever_email
+    message["BCC"] = sender_email
+
+    message.set_content(
+        f"""\
+        Hi {name},
+        I hope you are well. 
+        I just wanted to drop you a quick note to remind you that {amount} pesos in respect of our invoice {invoice_no} is due for payment on {due_date}. 
+        I would be really grateful if you could confirm that everything is on track for payment. 
+        Best regards 
+        JHOBERT
+        """
+    )
+    
+
 
 
 
