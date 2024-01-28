50511a1675ba54c90d98f3e120846e5a3159d221
Jhobert Buno <jhobertbuno02valdez@gmail.com>
2024-01-28 20:22:08 +0800

    load the environment

--- final_project_try.py
+++ final_project_try.py
@@ -7,3 +7,10 @@ from pathlib import Path
 
 from dotenv import load_dotenv #pip install python-dotenv
 
+port = 587
+email_server = "smtp.gmail.com"
+
+#load the environment
+current_dir = Path(__file__).resolve().parent if "__file__" in locals() else Path.cwd()
+envars = current_dir / ".env"
+load_dotenv(envars)
